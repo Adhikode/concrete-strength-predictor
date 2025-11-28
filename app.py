37da92a9c28f6ae80cbc607e007e97e58cc442ad
Niraj Adhikari <na765@njit.edu>
2025-11-28 13:49:47 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -51,13 +51,15 @@ col1, col2 = st.columns(2)
 with col1:
     st.subheader("Base Materials")
     for col in left_cols:
-        inputs[col] = st.number_input(col, min_value=0.0, value=0.0, format="%.0f")
+        value = st.text_input(col, value="0")
+        inputs[col] = float(value) if value.strip() else 0.0
 
 # RIGHT COLUMN INPUTS
 with col2:
     st.subheader("Supplementary & Special Materials")
     for col in right_cols:
-        inputs[col] = st.number_input(col, min_value=0.0, value=0.0, format="%.0f")
+        value = st.text_input(col, value="0")
+        inputs[col] = float(value) if value.strip() else 0.0
 
 # Concrete Type
 st.subheader("Concrete Type")
